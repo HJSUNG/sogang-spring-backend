25e7e4d0b572204457365d87240da27368a7c848
hojoon.sung <hjsung94@gmail.com>
2024-05-28 08:11:33 +0900

    Feat: 객체 판정 모델 관련 코드 추가

--- app/object_detection.py
+++ app/object_detection.py
@@ -5,8 +5,17 @@ from io import BytesIO
 from datetime import datetime
 import random # 테스트용 점수 생성
 import pytz
+import numpy as np
+
+from tensorflow.keras.models import load_model
+
 
 objectDetection_blueprint = Blueprint('objectDetection', __name__, url_prefix='/api/objectDetection')
+
+# 모델 및 레이블 파일 로드
+model = load_model('models/sleeping/keras_model.h5')
+with open('models/sleeping/labels.txt', 'r') as f:
+    labels = f.read().splitlines()
 
 @objectDetection_blueprint.route('/sendImage', methods=['POST'])
 def sendImage():
@@ -16,7 +25,6 @@ def sendImage():
     data = request.get_json()
 
     image_data_str = data['image']
-    # print(image_data_str)
 
     # base64 디코딩하여 이미지로 변환
     _, encoded = image_data_str.split(',', 1)
@@ -24,17 +32,29 @@ def sendImage():
 
     image_pil = Image.open(BytesIO(image_data))
 
-    # image_pil.show()
+    # 이미지를 RGB로 변환
+    image_pil = image_pil.convert('RGB')
+    # 이미지를 모델이 예상하는 크기로 조정 (예시: 224x224)
+    image_pil = image_pil.resize((224, 224))
 
-    # 학습된 Yolo Model들을 통한 자세 판정
+    # 이미지를 numpy 배열로 변환
+    img_array = np.array(image_pil)
 
-    # 판정 결과 정보를 DB에 저장
+    # 모델로 예측
+    predictions = model.predict(np.array([img_array]))
+
+    # 예측 결과 해석
+    predicted_label = labels[np.argmax(predictions)]
+
+    print(predicted_label)
+    print(predictions)
+    # 판정 결과 정보를 DB에 저장 (이 부분은 생략)
 
     return jsonify({
         'success': True,
         'judge_result': {
             'judge_dttm': now.strftime("%Y-%m-%d %H:%M:%S"),
-            'case1': round(random.random(),3)
+            'predicted_label': predicted_label
         },
-
-        'message': 'Image received and processed successfully'}), 200
+        'message': 'Image received and processed successfully'
+    }), 200
